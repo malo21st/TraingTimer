0f44ac408377e726efd972a662fde67d8da9a2f7
malo21st <44739330+malo21st@users.noreply.github.com>
2025-05-01 09:42:29 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,112 @@
+import streamlit as st
+import time
+
+# Data
+plan_data = [
+{
+    "name": "かかと",
+    "sets": 10,
+    "count_down": 5,
+    "plan": [
+        {
+            "text": "かかと上げて！",
+            "time": 1,
+            "visibility": False,
+        },
+        {
+            "text": "キープ！",
+            "time": 3,
+            "visibility": True,
+        },
+        {
+            "text": "下げて",
+            "time": 1,
+            "visibility": False,
+        },
+    ],
+},
+{
+    "name": "かたあし",
+    "sets": 5,
+    "count_down": 5,
+    "plan": [
+        {
+            "text": "右足上げて！",
+            "time": 1,
+            "visibility": False,
+        },
+        {
+            "text": "キープ！",
+            "time": 60,
+            "visibility": True,
+        },
+        {
+            "text": "下げて",
+            "time": 1,
+            "visibility": False,
+        },
+        {
+            "text": "左足上げて！",
+            "time": 1,
+            "visibility": False,
+        },
+        {
+            "text": "キープ！",
+            "time": 60,
+            "visibility": True,
+        },
+        {
+            "text": "下げて",
+            "time": 1,
+            "visibility": False,
+        },
+    ],
+}
+]
+
+def make_plan(plan_dic):
+    plan = []
+    for training in plan_dic:
+        name = training["name"]      
+        for num in range(training["count_down"], -1, -1):
+            plan.append(["カウントダウン", num, 0, name])
+        for sets_num in range(1, training["sets"]+1):
+            for p in training["plan"]:
+                for count in range(p["time"]+1):
+                    if p["visibility"]:
+                        plan.append([p["text"], count, sets_num, name])
+                    else:
+                        plan.append([p["text"], "　", sets_num, name])
+    return plan
+
+plan = make_plan(plan_data)
+
+# Streamlit
+## View
+if "started" not in st.session_state:
+    st.session_state.started = False
+if st.button("スタート"):
+    st.session_state.started = True
+
+main_msg = st.empty()
+
+## Logic
+counter = 0
+if st.session_state.started:
+    end_msg = st.empty()
+    for counter in range(len(plan)):
+        text, count, sets, name = plan[counter]
+        main_msg.markdown(
+            f"""
+            <h1 style='color: red;'>{text}</h1>
+            <div style='text-align: center;'>
+                <span style='font-size: 72px; color: lightgreen;'>{count}</span>
+            </div>
+            <h1 style='color: lightblue;'>{name}　{sets}</h1>
+            """,
+            unsafe_allow_html=True
+        )
+        time.sleep(1)
+        counter += 1
+    end_msg.markdown(f"<h1>お疲れさまでした</h1>", unsafe_allow_html=True)
+    st.session_state.started = False
