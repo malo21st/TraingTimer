2b39ea9435cea2f579f367e4fc189b3ca9405316
malo21st <44739330+malo21st@users.noreply.github.com>
2025-05-04 16:57:48 +0900

    Update ex_data.py

--- ex_data.py
+++ ex_data.py
@@ -67,13 +67,41 @@ _tmpl_ex1 = string.Template("""
 }
 """)
 
+# スクワット
+_tmpl_ex2 = string.Template("""
+{
+    "name": "スクワット",
+    "sets": ${sets_num},
+    "start_count": 5,
+    "plan": [
+        {
+            "text": "腰をゆっくり下げて！",
+            "time": 2,
+            "count": 0,
+        },
+        {
+            "text": "キープ！",
+            "time": ${keep_num},
+            "count": -1,
+        },
+        {
+            "text": "ゆっくり上げて",
+            "time": 2,
+            "count": 0,
+        },
+    ],
+}
+""")
+
 _ui_ex0 = {
     "name": "かかと上げ下ろし",
+    "image": "00_tsumasaki.png",
+    "available": True,
     "sets_def": 10,
     "sets_min": 5,
     "sets_max": 50,
     "sets_step": 5,
-    "keep_def": 5,
+    "keep_def": 10,
     "keep_min": 5,
     "keep_max": 60,
     "keep_step": 5,
@@ -81,6 +109,7 @@ _ui_ex0 = {
 
 _ui_ex1 = {
     "name": "片足立ち",
+    "available": True,
     "sets_def": 5,
     "sets_min": 5,
     "sets_max": 50,
@@ -91,9 +120,24 @@ _ui_ex1 = {
     "keep_step": 30,
 }
 
+_ui_ex2 = {
+    "name": "スクワット",
+    "available": False,
+    "sets_def": 10,
+    "sets_min": 5,
+    "sets_max": 50,
+    "sets_step": 5,
+    "keep_def": 3,
+    "keep_min": 3,
+    "keep_max": 15,
+    "keep_step": 3,
+}
+
+# 運動データリスト
 ex_data_list = [
     {"template": _tmpl_ex0, "ui": _ui_ex0,},
     {"template": _tmpl_ex1, "ui": _ui_ex1,},
+    {"template": _tmpl_ex2, "ui": _ui_ex2,},
 ]
 
 # メイン画面　メッセージ
